a2e0a40c06243461ea3ce778deacb30f0052658c
keshav660 <keshavbhatt660@gmail.com>
2026-06-04 07:51:41 +0530

    added repository list component

---
+++ client/src/components/RepoList.jsx
@@ -0,0 +1,16 @@
+import RepoCard from "./RepoCard";
+
+function RepoList({ repos }) {
+  return (
+    <div className="mt-8 space-y-4">
+      {repos.map((repo) => (
+        <RepoCard
+          key={repo.id}
+          repo={repo}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default RepoList;
