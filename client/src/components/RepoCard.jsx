b4ba4728a0498c92a388e9bc8b2b60ab07056ae4
keshav660 <keshavbhatt660@gmail.com>
2026-06-04 07:46:59 +0530

    created demo repository card component

---
+++ client/src/components/RepoCard.jsx
@@ -0,0 +1,33 @@
+function RepoCard({ repo }) {
+  return (
+    <div className="border rounded-xl p-5 shadow-sm">
+      
+      <h3 className="text-xl font-semibold">
+        {repo.name}
+      </h3>
+
+      <p className="text-gray-600 mt-2">
+        {repo.description || "No description available"}
+      </p>
+
+      <div className="flex flex-wrap gap-4 mt-4 text-sm">
+        
+        <span>
+          💻 {repo.language || "Unknown"}
+        </span>
+
+        <span>
+          ⭐ {repo.stargazers_count}
+        </span>
+
+        <span>
+          Updated: {repo.updated_at}
+        </span>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default RepoCard;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import SearchBar from "../components/SearchBar";
 import ProfileCard from "../components/ProfileCard";
+import RepoCard from "../components/RepoCard";
 const demoProfile = {
   avatar_url: "https://avatars.githubusercontent.com/u/108389536?s=400&u=192478f371a67d04bbbcd207cd843aaee1157c5e&v=4",
   name: "keshav bhatt",
@@ -7,6 +8,13 @@ const demoProfile = {
   followers: 25000,
   following: 4,
   public_repos: 40,
+};
+const demoRepo = {
+  name: "react",
+  description: "A JavaScript library for building user interfaces",
+  language: "JavaScript",
+  stargazers_count: 235000,
+  updated_at: "2026-06-01",
 };
 function Home() {
   const handleSearch = (username) => {
@@ -21,6 +29,7 @@ function Home() {
 
       <SearchBar onSearch={handleSearch} />
       <ProfileCard profile={demoProfile} />
+      <RepoCard repo={demoRepo} />
 
     </div>
   );
