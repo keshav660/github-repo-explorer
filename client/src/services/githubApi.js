d45a503d053a93439bee67d5b8f93f89b60b42b4
keshav660 <keshavbhatt660@gmail.com>
2026-06-04 20:46:34 +0530

    update production api url

--- client/src/services/githubApi.js
+++ client/src/services/githubApi.js
@@ -1,14 +1,9 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
-export const fetchGithubUser = async (
-  username
-) => {
-  const response = await axios.get(
-    `${API_BASE_URL}/${username}`
-  );
+export const fetchGithubUser = async (username) => {
+  const response = await axios.get(`${API_BASE_URL}/${username}`);
 
   return response.data;
 };
